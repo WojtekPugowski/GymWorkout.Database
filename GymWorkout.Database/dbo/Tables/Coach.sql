66cf735c9557a62c0eb21e21910ac488928488de
Wojtek Pugowski <Wojtek@BootCamp>
2023-10-01 12:45:46 +0200

    Added Coach and Exercise classes

---
+++ GymWorkout.Database/dbo/Tables/Coach.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Coach]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Surname] NVARCHAR(50) NULL, 
+    [Participants] NVARCHAR(50) NOT NULL
+)
---
+++ GymWorkout.Database/dbo/Tables/Exercise.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Exercise]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(50) NOT NULL, 
+    [NumberOfSeries] INT NULL, 
+    [NumberOfRepetitions] INT NULL, 
+    [WeightLifted] INT NULL, 
+    [Duration] INT NULL
+)
