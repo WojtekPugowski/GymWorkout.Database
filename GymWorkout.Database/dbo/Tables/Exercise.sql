1161aed087aae8c8be3a857cfd191109f94e6508
WojtekPugowski <wojtek@pugowski.pl>
2023-10-21 15:11:00 +0200

    Added ExerciseVatraibles table and relations to Exercise

--- GymWorkout.Database/dbo/Tables/Exercise.sql
+++ GymWorkout.Database/dbo/Tables/Exercise.sql
@@ -2,13 +2,13 @@
 (
 	[Id] INT NOT NULL IDENTITY(1,1),
 	[Title] NVARCHAR(50) NOT NULL,
-	[NumberOfSeries] INT NULL,
-	[NumberOfRepetitions] INT NULL,
-	[WeightLifted] INT NULL,
-	[Duration] INT NULL,
+	[ExercisePlannedId] INT NOT NULL,
+	[ExerciseDoneId] INT NOT NULL,
 	[TrainingDayId] INT NOT NULL,
 	CONSTRAINT PK_Exercise PRIMARY KEY (Id),
-	CONSTRAINT FK_Exercise_TrainingDay FOREIGN KEY (TrainingDayId) REFERENCES TrainingDay (Id)
+	CONSTRAINT FK_Exercise_ExercisePlanned FOREIGN KEY ([ExercisePlannedId]) REFERENCES ExerciseVariables(Id),
+	CONSTRAINT FK_Exercise_ExerciseDone FOREIGN KEY (ExerciseDoneId) REFERENCES ExerciseVariables(Id),
+	CONSTRAINT FK_Exercise_TrainingDay FOREIGN KEY (TrainingDayId) REFERENCES TrainingDay(Id)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 )
---
+++ GymWorkout.Database/dbo/Tables/ExerciseVariables.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ExerciseVariables]
+(
+	[Id] INT NOT NULL IDENTITY(1,1),
+	[NumberOfSeries] INT NULL,
+	[NumberOfRepetitions] INT NULL,
+	[WeightLifted] INT NULL,
+	[Duration] INT NULL,
+	CONSTRAINT PK_ExerciseVariables PRIMARY KEY (Id)
+)
