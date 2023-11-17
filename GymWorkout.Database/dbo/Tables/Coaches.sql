e45eb770bbf901932debd3b90560ca261a8a1763
WojtekPugowski <wojtek@pugowski.pl>
2023-11-17 16:36:51 +0100

    Changed names of tables. Added missing record

---
+++ GymWorkout.Database/dbo/Tables/Coaches.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Coaches]
+(
+	[Id] INT NOT NULL IDENTITY(1,1),
+    [Name] NVARCHAR(150) NOT NULL, 
+    [Surname] NVARCHAR(150) NULL,
+    [ParticipantId] INT NOT NULL,
+    CONSTRAINT PK_CoachId PRIMARY KEY (Id),
+    CONSTRAINT FK_Coach_Participant FOREIGN KEY (ParticipantId) REFERENCES Participants(Id)
+
+)
--- GymWorkout.Database/dbo/Tables/TrainingDays.sql
+++ GymWorkout.Database/dbo/Tables/TrainingDays.sql
@@ -1,10 +1,10 @@
-﻿CREATE TABLE [dbo].[TrainingDay]
+﻿CREATE TABLE [dbo].[TrainingDays]
 (
 	[Id] INT NOT NULL IDENTITY(1,1), 
     [DateAndTime] DATETIMEOFFSET NOT NULL,
 	[ParticipantId] INT NOT NULL, 
 	CONSTRAINT PK_TrainingDay PRIMARY KEY (Id), 
-	CONSTRAINT FK_TrainingDay_Participant FOREIGN KEY (ParticipantId) REFERENCES Participant(Id)
+	CONSTRAINT FK_TrainingDay_Participant FOREIGN KEY (ParticipantId) REFERENCES Participants(Id)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 )
